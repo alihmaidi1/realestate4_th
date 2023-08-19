8a9c814b3a6f2ae7accc5c0470f29eeefe864ec5
= <bshardark@gmail.com>
2023-08-19 09:49:11 +0300

    edit gender

--- app/Http/Requests/Api/Admin/Operations/UpdateAdminRequest.php
+++ app/Http/Requests/Api/Admin/Operations/UpdateAdminRequest.php
@@ -28,7 +28,7 @@ class UpdateAdminRequest extends FormRequest
       "name" => "required",
       "email" => "email|unique:users,email," . aauth()->id,
       "phone" => "required",
-      'gender' => 'required|in:male,female,other',
+      'gender' => 'sometimes|in:male,female,other',
       'role_id' => 'required|exists:roles,id',
       "image" =>  'image',
     ];
